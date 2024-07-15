2f5a237ce9d4b03aa06969ffa73569663172adce
Maximilien Brunet <maximilien.brunet@me.com>
2024-07-15 15:18:21 +0200

    installed router, finished form structure, imported fonts

---
+++ src/pages/RSVP.jsx
@@ -0,0 +1,14 @@
+import RSVPForm from "../components/RSVPForm";
+import styled from "styled-components";
+
+export default function RSVP() {
+  return (
+    <>
+      <Wrapper>
+        <RSVPForm />
+      </Wrapper>
+    </>
+  );
+}
+
+const Wrapper = styled.div``;
