a3dd214575b9fedd24a0b150f966c3191d47ff78
Maximilien Brunet <maximilien.brunet@me.com>
2024-07-19 17:43:51 +0200

    made rsvpform a card on top of background image

--- src/pages/RSVP.jsx
+++ src/pages/RSVP.jsx
@@ -1,62 +1,29 @@
 import RSVPForm from "../components/RSVPForm";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import LanguageSelector from "../components/LanguageSelector";
-import { QUERIES } from "../constants.js";
+import backgroundImage from "../media/backgroundRSVP.jpeg";
 
 export default function RSVP() {
-  const { t } = useTranslation();
-
   return (
     <>
       <Wrapper>
-        <Header>
-          <RSVPLanguageSelector />
-          <Title to="/RSVP">R.S.V.P.</Title>
-          <FinalDate>{t("RSVP.FinalDate")}</FinalDate>
-        </Header>
         <RSVPForm />
-        <HomeLink to="/">Back to Home Page</HomeLink>
+        {/* <HomeLink to="/">Back to Home Page</HomeLink> */}
       </Wrapper>
     </>
   );
 }
 
-const Wrapper = styled.div``;
-
-const RSVPLanguageSelector = styled(LanguageSelector)`
-  opacity: 0.3;
-`;
-
-const Header = styled.header`
+const Wrapper = styled.div`
+  background-image: url(${backgroundImage});
+  background-size: cover;
+  background-position: center;
+  min-height: 100vh; /* Ensure the background covers the entire viewport */
   display: flex;
-  flex-direction: column;
   align-items: center;
-  font-family: "Playwrite CU", cursive;
-  font-optical-sizing: auto;
-  font-weight: 400;
-  font-style: normal;
+  justify-content: center;
 `;
 
-const Title = styled(Link)`
-  cursor: pointer;
-  text-decoration: none;
-  color: var(--color-lighter-blue);
-  font-size: 3rem;
-  padding: 1rem 0 0;
-
-  @media ${QUERIES.laptopAndUp} {
-    font-size: 4rem;
-  }
-`;
-
-const FinalDate = styled.p`
-  text-align: center;
-  color: var(--color-lighter-blue);
-`;
-
-const HomeLink = styled(Link)`
+/* const HomeLink = styled(Link)`
   display: flex;
   justify-content: center;
   font-family: "Playwrite CU", cursive;
@@ -73,4 +40,4 @@ const HomeLink = styled(Link)`
     transition: all 0.4s ease-in-out;
     color: var(--color-dark-blue);
   }
-`;
+`; */
