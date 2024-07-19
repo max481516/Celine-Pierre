3532a6c5c9b36a2193bf4edcaebf73bc9e563f30
Maximilien Brunet <maximilien.brunet@me.com>
2024-07-19 15:52:59 +0200

    i18n full translation added for rsvp

--- src/pages/RSVP.jsx
+++ src/pages/RSVP.jsx
@@ -1,12 +1,21 @@
 import RSVPForm from "../components/RSVPForm";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import LanguageSelector from "../components/LanguageSelector";
+import { QUERIES } from "../constants.js";
 
 export default function RSVP() {
+  const { t } = useTranslation();
+
   return (
     <>
       <Wrapper>
-        <Title to="/RSVP">R.S.V.P.</Title>
+        <Header>
+          <RSVPLanguageSelector />
+          <Title to="/RSVP">R.S.V.P.</Title>
+          <FinalDate>{t("RSVP.FinalDate")}</FinalDate>
+        </Header>
         <RSVPForm />
         <HomeLink to="/">Back to Home Page</HomeLink>
       </Wrapper>
@@ -16,18 +25,35 @@ export default function RSVP() {
 
 const Wrapper = styled.div``;
 
-const Title = styled(Link)`
-  cursor: pointer;
-  text-decoration: none;
+const RSVPLanguageSelector = styled(LanguageSelector)`
+  opacity: 0.3;
+`;
+
+const Header = styled.header`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   font-family: "Playwrite CU", cursive;
   font-optical-sizing: auto;
   font-weight: 400;
   font-style: normal;
+`;
+
+const Title = styled(Link)`
+  cursor: pointer;
+  text-decoration: none;
   color: var(--color-lighter-blue);
   font-size: 3rem;
-  padding: 2rem 0 0;
+  padding: 1rem 0 0;
+
+  @media ${QUERIES.laptopAndUp} {
+    font-size: 4rem;
+  }
+`;
+
+const FinalDate = styled.p`
+  text-align: center;
+  color: var(--color-lighter-blue);
 `;
 
 const HomeLink = styled(Link)`
